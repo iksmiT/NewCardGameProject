84eb8c6020037b8105713a136a7f6ffb82ef4615
iksmiT <tim.vongsana@hotmail.fr>
2016-03-08 00:39:18 +0100

    True rules, two decks but no card swipe after winning a round.

--- app/src/main/java/timski_ensicaen/newcardgameproject/PlayActivity.java
+++ app/src/main/java/timski_ensicaen/newcardgameproject/PlayActivity.java
@@ -36,6 +36,7 @@ public class PlayActivity extends AppCompatActivity {
         scorePlayerOne = (TextView) findViewById(R.id.score_one_text);
         scorePlayerTwo = (TextView) findViewById(R.id.score_two_text);
         endText = (TextView) findViewById(R.id.end_text);
+
         initGame(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
     }
 
@@ -47,7 +48,8 @@ public class PlayActivity extends AppCompatActivity {
         pickDeckButtonOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cardOne = mCardDeck.pickCardEntity();
+                //cardOne = mCardDeck.pickCardEntity();
+                cardOne = mCardDeck.pickCardEntity(mCardDeck.getPlayer_one_deck());
                 ImageView cardImage = (ImageView) findViewById(R.id.image_view_one);
                 TextView playerOneText = (TextView) findViewById(R.id.text_one);
                 TextView playerTwoText = (TextView) findViewById(R.id.text_two);
@@ -66,7 +68,8 @@ public class PlayActivity extends AppCompatActivity {
         pickDeckButtonTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                cardTwo = mCardDeck.pickCardEntity();
+                //cardTwo = mCardDeck.pickCardEntity();
+                cardTwo = mCardDeck.pickCardEntity(mCardDeck.getPlayer_two_deck());
                 cardImageTwo = (ImageView) findViewById(R.id.image_view_two);
                 TextView playerOneText = (TextView) findViewById(R.id.text_one);
                 TextView playerTwoText = (TextView) findViewById(R.id.text_two);
@@ -91,12 +94,17 @@ public class PlayActivity extends AppCompatActivity {
                 Button pickDeckButtonOne = (Button) findViewById(R.id.pick_from_one);
                 Button pickDeckButtonTwo = (Button) findViewById(R.id.pick_from_two);
 
-                if (mRound.getPhase() != Round.FINAL_PHASE) {
+                //if (mRound.getPhase() != Round.FINAL_PHASE) {
+                if ((mRound.getPhase() == Round.PICK_PHASE)) {
                     newRound(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
-                    initGame(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
                     //turnSystem(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
+                } else if ((mRound.getPhase() == Round.BATTLE_PHASE)) {
+                    newRound(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
+                    turnSystem(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
+                } else if (mRound.getPhase() == Round.WINNER_PHASE) {
+
                 } else {
-                    resetGame(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
+                    //resetGame(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
                     initGame(mRound, pickDeckButtonOne, pickDeckButtonTwo, playerOneText, playerTwoText);
                 }
             }
@@ -139,13 +147,15 @@ public class PlayActivity extends AppCompatActivity {
     }
 
     public void initGame(Round round, Button buttonOne, Button buttonTwo, TextView mTextView1, TextView mTextView2) {
-        scorePlayerOne.setText(String.valueOf(round.getPlayerOneScore()));
-        scorePlayerTwo.setText(String.valueOf(round.getPlayerTwoScore()));
+        mCardDeck = new CardDeck();
+        mRound = new Round();
+        scorePlayerOne.setText(String.valueOf(mRound.getPlayerOneScore()));
+        scorePlayerTwo.setText(String.valueOf(mRound.getPlayerTwoScore()));
         cardImageOne.setImageResource(android.R.color.transparent);
         cardImageTwo.setImageResource(android.R.color.transparent);
         switchTurnButton.setText("NEXT ROUND");
         endText.setText("");
-        if (round.getTurn() == Round.PLAYER_ONE_TURN) {
+        if (mRound.getTurn() == Round.PLAYER_ONE_TURN) {
             buttonTwo.setEnabled(false);
             buttonOne.setEnabled(true);
             mTextView1.setText("PLAYER ONE TURN !!!");
@@ -158,6 +168,32 @@ public class PlayActivity extends AppCompatActivity {
             mTextView1.setText("");
         }
         switchTurnButton.setVisibility(View.GONE);
+        Log.d("INIT", "INIT GAME " + "PHASE : " + String.valueOf(mRound.getPhase()) + "TURN OF " + String.valueOf(mRound.getTurn()));
+    }
+
+
+    public void newRound(Round round, Button buttonOne, Button buttonTwo, TextView mTextView1, TextView mTextView2) {
+        mRound.setPlayerOnePlayed(false);
+        mRound.setPlayerTwoPlayed(false);
+        //mRound.setPhase(Round.PICK_PHASE);
+        scorePlayerOne.setText(String.valueOf(mRound.getPlayerOneScore()));
+        scorePlayerTwo.setText(String.valueOf(mRound.getPlayerTwoScore()));
+        cardImageOne.setImageResource(android.R.color.transparent);
+        cardImageTwo.setImageResource(android.R.color.transparent);
+        if (mRound.getTurn() == Round.PLAYER_ONE_TURN) {
+            buttonTwo.setEnabled(false);
+            buttonOne.setEnabled(true);
+            mTextView1.setText("PLAYER ONE TURN !!!");
+            mTextView2.setText("");
+
+        } else {
+            buttonOne.setEnabled(false);
+            buttonTwo.setEnabled(true);
+            mTextView2.setText("PLAYER TWO TURN !!!");
+            mTextView1.setText("");
+        }
+        switchTurnButton.setVisibility(View.GONE);
+        Log.d("NEW ROUND", "PHASE : " + String.valueOf(mRound.getPhase()) + " play_one_played = " + String.valueOf(mRound.getPlayerOnePlayed()) + " player_two_played = " + String.valueOf(mRound.getPlayerTwoPlayed()));
     }
 
     public void resetGame(Round round, Button buttonOne, Button buttonTwo, TextView mTextView1, TextView mTextView2) {
@@ -166,36 +202,59 @@ public class PlayActivity extends AppCompatActivity {
     }
 
     public void turnSystem(Round round, Button buttonOne, Button buttonTwo, TextView mTextView1, TextView mTextView2) {
-        Log.d("TURN SYSTEM","phase = " + String.valueOf(round.getPhase()) + " play_one_played = " + String.valueOf(round.getPlayerOnePlayed()) + " player_two_played = " + String.valueOf(round.getPlayerTwoPlayed()));
-        if ( (round.getPhase() == Round.PICK_PHASE) && !((round.getPlayerOnePlayed()) && (round.getPlayerTwoPlayed()))) {
+        Log.d("TURN SYSTEM", "PHASE : " + String.valueOf(mRound.getPhase()));
+        int winner;
+        if ((round.getPhase() == Round.PICK_PHASE)) {
+            Log.d("PICK PHASE", "PHASE : " + String.valueOf(mRound.getPhase()));
+
             if (round.getTurn() == Round.PLAYER_ONE_TURN) {
+                Log.d("PICK PHASE", "PHASE : " + String.valueOf(mRound.getPhase()) + "| PLAYER ONE PICK " + "| PLAYER ONE STATE : " + String.valueOf(mRound.getPlayerOnePlayed()) + "| PLAYER TWO STATE : " + String.valueOf(mRound.getPlayerTwoPlayed()));
                 buttonOne.setEnabled(false);
                 buttonTwo.setEnabled(true);
                 mTextView2.setText("PLAYER TWO TURN !!!");
                 mTextView1.setText("");
                 round.setTurn(Round.PLAYER_TWO_TURN);
                 round.setPlayerOnePlayed(true);
-            } else {
+                Log.d("PICK PHASE", "PHASE : " + String.valueOf(mRound.getPhase()) + "| PLAYER ONE PICK " + "| PLAYER ONE STATE : " + String.valueOf(mRound.getPlayerOnePlayed()) + "| PLAYER TWO STATE : " + String.valueOf(mRound.getPlayerTwoPlayed()));
+            }
+            else if (round.getTurn() == Round.PLAYER_TWO_TURN) {
+                Log.d("PICK PHASE", "PHASE : " + String.valueOf(mRound.getPhase()) + "| PLAYER ONE PICK " + "| PLAYER ONE STATE : " + String.valueOf(mRound.getPlayerOnePlayed()) + "| PLAYER TWO STATE : " + String.valueOf(mRound.getPlayerTwoPlayed()));
                 buttonTwo.setEnabled(false);
                 buttonOne.setEnabled(true);
                 mTextView1.setText("PLAYER ONE TURN !!!");
                 mTextView2.setText("");
                 round.setTurn(Round.PLAYER_ONE_TURN);
                 round.setPlayerTwoPlayed(true);
+                Log.d("PICK PHASE", "PHASE : " + String.valueOf(mRound.getPhase()) + "| PLAYER ONE PICK " + "| PLAYER ONE STATE : " + String.valueOf(mRound.getPlayerOnePlayed()) + "| PLAYER TWO STATE : " + String.valueOf(mRound.getPlayerTwoPlayed()));
             }
 
-            if ((round.getPlayerOnePlayed()) && (round.getPlayerTwoPlayed())) {
-                round.setPhase(Round.BATTLE_PHASE);
-
+            if (round.getPlayerOnePlayed() && round.getPlayerTwoPlayed()) {
+                Log.d("PICK PHASE", "PHASE : " + String.valueOf(mRound.getPhase()) + " SWITCH TO BATTLE PHASE ");
+                //round.setPhase(Round.BATTLE_PHASE);
                 buttonOne.setEnabled(false);
                 buttonTwo.setEnabled(false);
-                mTextView1.setText("");
-                mTextView2.setText("");
 
-               // if (cardOne == null || cardTwo == null) {
-                if ((mCardDeck.getAvail_deck().size() == 0)) {
-                    mTextView1.setText("");
-                    mTextView2.setText("");
+                winner = CardEntity.cardBattle(cardOne, cardTwo);
+                Log.d("BATTLE PHASE", "phase = " + mRound.getPhase() + " play_one_played = " + String.valueOf(cardOne.getId()) + " player_two_played = " + String.valueOf(cardTwo.getId()) + " winner = " + String.valueOf(winner));
+                if (winner == Round.PLAYER_ONE_WINS) {
+                    mTextView1.setText("PLAY ONE SCORES !");
+                    mTextView2.setText("PLAY TWO LOSES !");
+                    mRound.setPlayerOneScore(mRound.getPlayerOneScore() + mRound.getPoint());
+                    scorePlayerOne.setText(String.valueOf(mRound.getPlayerOneScore()));
+                    mRound.setPoint(1);
+                } else if (winner == Round.PLAYER_TWO_WINS) {
+                    mTextView2.setText("PLAY TWO SCORES !");
+                    mTextView1.setText("PLAY ONE LOSES !");
+                    mRound.setPlayerTwoScore(mRound.getPlayerTwoScore() + mRound.getPoint());
+                    scorePlayerTwo.setText(String.valueOf(mRound.getPlayerTwoScore()));
+                    mRound.setPoint(1);
+                } else {
+                    mTextView2.setText("DRAW !");
+                    mTextView1.setText("DRAW !");
+                    mRound.setPoint(mRound.getPoint() + 1);
+                }
+                switchTurnButton.setText("NEXT ROUND");
+                if ((mCardDeck.getPlayer_one_deck().size()) == 0 && (mCardDeck.getPlayer_one_deck().size() == 0)) {
                     if (round.getPlayerOneScore() > round.getPlayerTwoScore()) {
                         endText.setText("PLAYER 1 WINS");
                     }
@@ -206,42 +265,11 @@ public class PlayActivity extends AppCompatActivity {
                     switchTurnButton.setText("REMATCH");
                     mRound.setPhase(Round.FINAL_PHASE);
                 }
-                else {
-                    int winner = CardEntity.cardBattle(cardOne, cardTwo);
-                    Log.d("BATTLE PHASE", "phase = " + round.getPhase() + " play_one_played = " + String.valueOf(cardOne.getId()) + " player_two_played = " + String.valueOf(cardTwo.getId()) + " winner = " + String.valueOf(winner));
-                    if (winner == Round.PLAYER_ONE_WINS) {
-                        mTextView1.setText("PLAY ONE WINS !");
-                        mTextView2.setText("PLAY TWO LOSES !");
-                        round.setPlayerOneScore(round.getPlayerOneScore() + round.getPoint());
-                        scorePlayerOne.setText(String.valueOf(round.getPlayerOneScore()));
-                        round.setPoint(1);
-                    } else if (winner == Round.PLAYER_TWO_WINS) {
-                        mTextView2.setText("PLAY TWO WINS !");
-                        mTextView1.setText("PLAY ONE LOSES !");
-                        round.setPlayerTwoScore(round.getPlayerTwoScore() + round.getPoint());
-                        scorePlayerTwo.setText(String.valueOf(round.getPlayerTwoScore()));
-                        round.setPoint(1);
-                    } else {
-                        mTextView2.setText("DRAW !");
-                        mTextView1.setText("DRAW !");
-                        round.setPoint(round.getPoint() + 1);
-                    }
-
-                }
                 switchTurnButton.setVisibility(View.VISIBLE);
             }
         }
-        Log.d("TURN SYSTEM","phase = " + String.valueOf(round.getPhase()) + " play_one_played = " + String.valueOf(round.getPlayerOnePlayed()) + " player_two_played = " + String.valueOf(round.getPlayerTwoPlayed()));
     }
 
-    public void newRound(Round round, Button buttonOne, Button buttonTwo, TextView mTextView1, TextView mTextView2) {
-        round.setPlayerOnePlayed(false);
-        round.setPlayerTwoPlayed(false);
-        round.setPhase(Round.PICK_PHASE);
-        cardImageOne.setImageResource(android.R.color.transparent);
-        cardImageTwo.setImageResource(android.R.color.transparent);
-        Log.d("NEW ROUND", "phase = " + String.valueOf(round.getPhase()) + " play_one_played = " + String.valueOf(round.getPlayerOnePlayed()) + " player_two_played = " + String.valueOf(round.getPlayerTwoPlayed()));
-    }
 
     public void displayCardEntity(ImageView imageView, int i, int customWidth, int customHeight) {
         switch (i) {
